be8291b099022ba36ad32e038e7ad39f227b9b50
PazyFlorasEnsayo <120591603+PazyFlorasEnsayo@users.noreply.github.com>
2022-12-19 02:29:52 -0300

    Add files via upload

--- webpazyfloras/urls.py
+++ webpazyfloras/urls.py
@@ -1,19 +1,42 @@
-from django.contrib import admin
-from django.conf import settings
-from django.views.static import serve
-from django.urls import path, include, re_path
-from tienda.views import index
-from gestiontienda.views import accesodenegado
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tienda/', include('tienda.urls')),
-    path('gestiontienda/', include('gestiontienda.urls')),
-    path('',index), 
-    path('accesodenegado',accesodenegado)
-]
+from django.urls import path, include
+from tienda.views import *
 
-urlpatterns+=[
-    re_path(r'^media/(?P<path>.*)$', serve, {
-        'document_root':settings.MEDIA_ROOT,
-    })
+app_name = "tienda"
+
+urlpatterns = [
+    #Public Info
+    path('index', index),
+    path('', index),
+    path('productos', productos),
+    path('vasijas', vasijas),
+    path('maceteros', maceteros),
+    path('plantas', plantas),
+    path('servicios', servicios),
+    path('quienessomos', quienessomos),
+    path('contacto', contacto),
+
+    #Accounts Session System 
+    path('cuenta/', include('django.contrib.auth.urls')),
+    
+    #Account Info
+    path('cuenta/carrito', carrito),
+    path('agregarProducto/<int:id>', agregarProducto),
+    path('eliminarProducto/<int:id>', eliminarProducto),
+    path('pedirCarrito', pedirCarrito), 
+    path('pedirServicio/<int:id>', pedirServicio), 
+    path('cuenta/perfil', perfil),
+    path('cuenta/registro', registro), 
+    path('cuenta/mispedidos', perfilpedidos), 
+    path('cuenta/verdetallepedido/<int:id>', verdetallepedido),
+    path('cuenta/detallepedido', perfildetallepedido),
+    path('cuenta/misservicios', perfilservicios),
+    path('cuenta/verdetalleservicio/<int:id>', verdetalleservicio),
+    path('cuenta/detalleservicio', perfildetalleservicio),
+    path('cuenta/pagarpedidoproductos/pagar/<int:id>', perfilpagarcarritopagar),
+    path('cuenta/pagarpedidoproductos/webpay/<int:id>', perfilpagarcarritowebpay),
+    path('cuenta/pagarpedidoproductos/pagado/<int:id>', perfilpagarcarritopagado),
+    path('cuenta/pagarpedidoservicio/pagar/<int:id>', perfilpagarserviciopagar),
+    path('cuenta/pagarpedidoservicio/webpay/<int:id>', perfilpagarserviciowebpay),
+    path('cuenta/pagarpedidoservicio/pagado/<int:id>', perfilpagarserviciopagado),
+    
 ]
